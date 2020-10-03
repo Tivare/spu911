10155a8230ba9ac85d13cdab82cc41f75c98edc4
Khylko Andrii <hilko_a@itstep.academy>
2020-10-03 11:03:26 +0300

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include"sum.h"
+
+using namespace std;
+
+int main()
+{
+	cout<<"Hello git"<<endl;
+	int a, b;
+	cout<<"Enter two numbers->";
+	cin>>a>>b;
+	int res = Sum(a,b);
+	cout<<"Res = "<<res<<endl;
+}
---
+++ sum.cpp
@@ -0,0 +1,5 @@
+#include"sum.h"
+int Sum(int a, int b)
+{
+	return a+b;
+}
